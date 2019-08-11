9ba8fa1b8b3763e823bd3e3ecf442b2b07c9deb9
invitationfaisal <53996476+invitationfaisal@users.noreply.github.com>
2019-08-12 00:04:58 +0700

    upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('#banner').backstretch([
+      "https://i.gyazo.com/d125aba42e90b08ace3ff4a614d04d54.png"
+    , "https://i.gyazo.com/497ea81b9ad2621ca9d3af96ca1ef661.png"
+    , "https://i.gyazo.com/d79c1c0e9ac7fd17987c9e2350e69289.png"
+    , "https://i.gyazo.com/709d45c455e9deb969d419e22207c33d.png"
+    ], {duration: 3000, fade: 750});
+});
